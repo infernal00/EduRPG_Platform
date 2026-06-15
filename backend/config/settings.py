056deb372e010f35b0e474c71aba9c0c4548a69f
lattice <zhbava@gmail.com>
2026-06-15 14:48:01 +0500

    Очистил папку и добавил приложении

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,10 +16,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    
     'core',
+    'users',
+    'courses',
+    'tools',
+    'shop',
 ]
 
 MIDDLEWARE = [
